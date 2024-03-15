d9f8e14f85e66bdb26656fa3eb36b918d76e8dd4
Markus Bernhardt <markus.bernhardt@scmb.de>
2024-03-15 09:38:03 +0100

    Fix some warnings

--- src/main/java/de/scmb/scotty/config/ProxyConfiguration.java
+++ src/main/java/de/scmb/scotty/config/ProxyConfiguration.java
@@ -9,21 +9,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProxyConfiguration {
 
-    private final ApplicationProperties applicationProperties;
-
     private final Logger log = LoggerFactory.getLogger(ProxyConfiguration.class);
 
     public ProxyConfiguration(ApplicationProperties applicationProperties) {
-        this.applicationProperties = applicationProperties;
-
-        if (this.applicationProperties.getProxy().getHost() != null) {
-            System.setProperty("http.proxyHost", this.applicationProperties.getProxy().getHost());
-            System.setProperty("http.proxyPort", Integer.toString(this.applicationProperties.getProxy().getPort()));
-            if (this.applicationProperties.getProxy().getNonProxyHosts() != null) {
-                System.setProperty("http.nonProxyHosts", this.applicationProperties.getProxy().getNonProxyHosts());
+        if (applicationProperties.getProxy().getHost() != null) {
+            System.setProperty("http.proxyHost", applicationProperties.getProxy().getHost());
+            System.setProperty("http.proxyPort", Integer.toString(applicationProperties.getProxy().getPort()));
+            if (applicationProperties.getProxy().getNonProxyHosts() != null) {
+                System.setProperty("http.nonProxyHosts", applicationProperties.getProxy().getNonProxyHosts());
             }
-            System.setProperty("https.proxyHost", this.applicationProperties.getProxy().getHost());
-            System.setProperty("https.proxyPort", Integer.toString(this.applicationProperties.getProxy().getPort()));
+            System.setProperty("https.proxyHost", applicationProperties.getProxy().getHost());
+            System.setProperty("https.proxyPort", Integer.toString(applicationProperties.getProxy().getPort()));
             System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
 
             Authenticator.setDefault(
